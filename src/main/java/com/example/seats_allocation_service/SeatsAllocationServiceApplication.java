d6bbf5dd5efe0a889cbc62336c34b387c2c0bb8d
Abhi <metromaxabhinav@gmail.com>
2026-03-03 23:08:32 +0530

    application initial setup changes

---
+++ src/main/java/com/example/seats_allocation_service/SeatsAllocationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.seats_allocation_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeatsAllocationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeatsAllocationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/seats_allocation_service/SeatsAllocationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seats_allocation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeatsAllocationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
